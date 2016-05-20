940976d3ec43ba86aa28151625328c2055dc1f1b
LAICHENGYU <ArthurLai@ArthurLaiMacBookAir13.local>
2016-05-20 10:57:34 +0800

    go Docker

---
+++ Dockerfile
@@ -0,0 +1,18 @@
+
+# Dockerfile
+
+# 從 [Docker Hub](https://hub.docker.com/) 安裝 Node.js image。
+FROM node:6.2.0
+
+# 設定 container 的預設目錄位置
+WORKDIR /hello-ci-workflow-7
+
+# 將專案根目錄的檔案加入至 container
+# 安裝 npm package
+ADD . /hello-ci-workflow-7
+RUN npm install
+
+# 開放 container 的 3000 port
+EXPOSE 3080
+CMD ["npm", "start"]
+
--- index.js
+++ index.js
@@ -4,10 +4,17 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello, Arthur!');
+  //res.send('Hello, Arthur!');
+  //res.send('this is Express ~');  
+
+  var date = new Date();
+  //var current_hour = date.getHours();
+  var current_hour = Date.now();
+
+  res.send('Hello, Arthur! <br>' + current_hour);
 });
 
-var server = app.listen(3000, function () {
+var server = app.listen(3080, function () {
 
   var host = server.address().address;
   var port = server.address().port;
